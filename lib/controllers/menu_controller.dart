21632336e068557f525a2895ed149dc0850395e1
Nguyen Gia Huy <nggiahuy09@gmail.com>
2024-07-14 22:17:21 +0700

    add: scaffold key and control for the orders menu

--- lib/controllers/menu_controller.dart
+++ lib/controllers/menu_controller.dart
@@ -6,11 +6,13 @@ class MenuControllers extends ChangeNotifier {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   final GlobalKey<ScaffoldState> _gridScaffoldKey = GlobalKey<ScaffoldState>();
   final GlobalKey<ScaffoldState> _addProductScaffoldKey = GlobalKey<ScaffoldState>();
+  final GlobalKey<ScaffoldState> _ordersScaffoldKey = GlobalKey<ScaffoldState>();
 
   // getters
   GlobalKey<ScaffoldState> get scaffoldKey => _scaffoldKey;
   GlobalKey<ScaffoldState> get gridScaffoldKey => _gridScaffoldKey;
   GlobalKey<ScaffoldState> get addProductScaffoldKey => _addProductScaffoldKey;
+  GlobalKey<ScaffoldState> get ordersScaffoldKey => _ordersScaffoldKey;
 
   // callbacks
   void controlDashboardMenu() {
@@ -22,6 +24,12 @@ class MenuControllers extends ChangeNotifier {
   void controlProductsMenu() {
     if(!_gridScaffoldKey.currentState!.isDrawerOpen) {
       _gridScaffoldKey.currentState!.openDrawer();
+    }
+  }
+
+  void controlOrdersMenu() {
+    if(!_ordersScaffoldKey.currentState!.isDrawerOpen) {
+      _ordersScaffoldKey.currentState!.openDrawer();
     }
   }
 
