1c32ecf5afecabff71ae5311eef9334c011c997f
Nguyen Gia Huy <nggiahuy09@gmail.com>
2024-07-28 21:21:57 +0700

    chore: add number input formatter

--- lib/services/utils.dart
+++ lib/services/utils.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:kb_shop_admin/provider/dark_theme_provider.dart';
 import 'package:provider/provider.dart';
 
-
 class Utils {
   BuildContext context;
+
   Utils(this.context);
 
   bool get getTheme => Provider.of<DarkThemeProvider>(context).getDarkTheme;
+
   Color get color => getTheme ? Colors.white : Colors.black;
+
   Size get getScreenSize => MediaQuery.of(context).size;
+
+  TextInputFormatter get numberInputFormatter => FilteringTextInputFormatter.allow(
+        RegExp(r'[0-9.]'),
+      );
 }
