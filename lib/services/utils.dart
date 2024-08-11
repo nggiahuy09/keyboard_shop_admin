b9c9ea01052ac13dc91cd9d51c16b19920d893b5
Nguyen Gia Huy <nggiahuy09@gmail.com>
2024-08-11 11:34:35 +0700

    add: show toast function utilities

--- lib/services/utils.dart
+++ lib/services/utils.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:kb_shop_admin/provider/dark_theme_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -17,4 +18,23 @@ class Utils {
   TextInputFormatter get numberInputFormatter => FilteringTextInputFormatter.allow(
         RegExp(r'[0-9.]'),
       );
+
+  static void showToast({
+    required String msg,
+    Toast toastLength = Toast.LENGTH_SHORT,
+    ToastGravity toastGravity = ToastGravity.BOTTOM,
+    Color background = Colors.red,
+    Color text = Colors.black12,
+    double fontSize = 16.0,
+  }) async {
+    await Fluttertoast.showToast(
+      msg: msg,
+      toastLength: toastLength,
+      gravity: toastGravity,
+      timeInSecForIosWeb: 1,
+      backgroundColor: background,
+      textColor: text,
+      fontSize: fontSize,
+    );
+  }
 }
