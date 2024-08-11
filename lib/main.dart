5a68a78786cf79a50fd5d588d375d461c0c78ccb
Nguyen Gia Huy <nggiahuy09@gmail.com>
2024-08-11 10:21:12 +0700

    feat: initialize connect to firebase services

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:kb_shop_admin/firebase_options.dart';
 import 'package:kb_shop_admin/provider/dark_theme_provider.dart';
 import 'package:kb_shop_admin/screens/main_page.dart';
 import 'package:provider/provider.dart';
 import 'consts/theme_data.dart';
 import 'package:kb_shop_admin/controllers/menu_controller.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
@@ -20,7 +27,8 @@ class _MyAppState extends State<MyApp> {
   DarkThemeProvider themeChangeProvider = DarkThemeProvider();
 
   void getCurrentAppTheme() async {
-    themeChangeProvider.setDarkTheme = await themeChangeProvider.darkThemePreference.getTheme();
+    themeChangeProvider.setDarkTheme =
+        await themeChangeProvider.darkThemePreference.getTheme();
   }
 
   @override
