090d4f89f2c3dbb4d52516c1b9f64ba902b94f4b
Nguyen Gia Huy <nggiahuy09@gmail.com>
2024-08-11 11:34:15 +0700

    add: utilities for firebase store and firebase storage

---
+++ lib/services/firebase_services.dart
@@ -0,0 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
+final fireStoreInstance = FirebaseFirestore.instance;
+final fireStorageInstance = FirebaseStorage.instance;
---
+++ lib/services/uuid_services.dart
@@ -0,0 +1,4 @@
+import 'package:uuid/uuid.dart';
+
+final uuidV1 = const Uuid().v1();
+final uuidV4 = const Uuid().v4();
